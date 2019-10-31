5c1971a4916b7f7b8ec4f57d7ad100483344724d
Niels Lerche Sørensen <nieslerches@gmail.com>
2019-10-31 08:02:28 +0100

    Median aggregation

--- shared/common_query/aggregations.py
+++ shared/common_query/aggregations.py
@@ -1,5 +1,3 @@
-from itertools import chain
-
 from shared.common_query import FilterableMixin, ArithmeticOperable, Comparable
 
 
@@ -27,6 +25,24 @@ class Mean(FilterableMixin, ArithmeticOperable, Comparable, Aggregation):
         result = Sum(self.field).where(self.query).reducer(queryset)
         result /= Count(self.field).where(self.query).reducer(queryset)
         return result
+
+
+class Median(FilterableMixin, ArithmeticOperable, Comparable, Aggregation):
+    def reducer(self, queryset):
+        values = list(sorted(queryset.compiler.get_value(object, self.field) for object in queryset))
+        length = len(values)
+
+        if length == 0:
+            return None
+
+        if length % 2 == 0:
+            right = length // 2
+            left = right - 1
+            right = values[right]
+            left = values[left]
+            return sum([right, left]) / 2
+
+        return values[length // 2]
 
 
 class Collect(FilterableMixin, ArithmeticOperable, Comparable, Aggregation):
